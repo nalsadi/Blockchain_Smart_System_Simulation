85d3830a6fb42f776681205e4b85e431b0f05c9b
nalsadi <nalsadi@uoguelph.ca>
2022-11-23 17:05:05 -0500

    BlockSim + Modifications

---
+++ Models/Network.py
@@ -0,0 +1,13 @@
+import random
+from InputsConfig import InputsConfig as p
+
+class Network:
+    def tx_prop_delay():
+        return random.expovariate(1/p.Tdelay)
+    
+    # Delay for propagating blocks in the network
+    def block_prop_delay():
+    	return random.expovariate(1/p.Bdelay)
+
+    # Delay for propagating transactions in the network
+    
